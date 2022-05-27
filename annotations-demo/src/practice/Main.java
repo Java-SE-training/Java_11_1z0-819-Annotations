1a86b2725b2e8243087796102baa023cba68f73f
Elimane Fofana <elimane.fofana@supinfo.com>
2022-05-27 14:46:25 +0200

    @SuppressWarnings annotation. Closes #3

--- annotations-demo/src/practice/Main.java
+++ annotations-demo/src/practice/Main.java
@@ -7,6 +7,7 @@ import practice.models.Person;
  * @PROJECT annotations-demo
  * @Author Elimane on 24/05/2022
  */
+@SuppressWarnings("deprecation")
 public class Main {
   public static void main(String[] args) {
       Person engineer = new Engineer(1L, "Eric Johnson");
