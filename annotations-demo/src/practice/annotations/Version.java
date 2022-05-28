d408456cb1c6e84894153cb9b6bafc7e2e60f11c
Elimane Fofana <elimane.fofana@supinfo.com>
2022-05-28 13:44:20 +0200

    @Target annotation. Closes #5

--- annotations-demo/src/practice/annotations/Version.java
+++ annotations-demo/src/practice/annotations/Version.java
@@ -1,13 +1,17 @@
 package practice.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * @PROJECT annotations-demo
  * @Author Elimane on 28/05/2022
  */
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.TYPE})
 public @interface Version {
   //Fields as a method
-  int value() default 1; // "default" keyword makes it  optional
-  String author(); // mandatory
+  int value(); // mandatory
+  String author() default "";  // "default" keyword makes it  optional
   String licence() default "MIT"; // "default" keyword makes it  optional
 
   //Array field
--- annotations-demo/src/practice/models/Engineer.java
+++ annotations-demo/src/practice/models/Engineer.java
@@ -6,11 +6,17 @@ import practice.annotations.Version;
  * @PROJECT annotations-demo
  * @Author Elimane on 24/05/2022
  */
-@Version(author = "John", environments = {"e1","e2"})
+@Version(value = 1,author = "John", environments = {"e1","e2"})
 public class Engineer extends Person{
+
+  //@Version(1) Not working
+  private String departement;
+
+  @Version(2)
   public Engineer() {
   }
 
+  @Version(3)
   public Engineer(Long id, String name) {
     super(id, name);
   }
